0b6cd38bd63b4caf903fe61f758e6e9f3cddbb46
MaximInnopolis <71584246+MaximInnopolis@users.noreply.github.com>
2022-03-20 16:01:02 +0300

    Created statistic for shortest path

--- Assignment1/src/MaximLatypov.java
+++ Assignment1/src/MaximLatypov.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class MaximLatypov {
 
     public static void main(String[] args) {
-        Statistic statistic = new Statistic(0,0,0,0,0,0,0,0,0);
+        Statistic statistic = new Statistic(0,0,0,
+                0,0,0,0,
+                0,0,0, 0);
 //        readFromConsole();
-        generateTest(statistic, 100000);
+        generateTest(statistic, 1000);
         statistic.printStatistic();
     }
 
@@ -167,18 +169,27 @@ public class MaximLatypov {
                 statistic.setAvTimeAStar(statistic.getAvTimeAStar() + HarryCopy.getTimeTaken());
             }
             Actor.findShortestPath(HarryCopy1, Filch, Cat, Book, Cloak, Exit, scenario);
+            if (Objects.equals(HarryCopy.getOutcome(), "Win")){
+                statistic.setWinOutcomeNumShPath(statistic.getWinOutcomeNumShPath() + 1);
+
+                statistic.setAvStepNumShPath(statistic.getAvStepNumShPath() + HarryCopy1.getStep());
+                statistic.setAvTimeShPath(statistic.getAvTimeShPath() + HarryCopy1.getTimeTaken());
+            }
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
 
         statistic.setTestsNumber(testsNumber);
         statistic.setProbWinBacktrack((double) statistic.getWinOutcomeNumBacktrack() / statistic.getTestsNumber());
         statistic.setProbWinAStar((double) statistic.getWinOutcomeNumAStar() / statistic.getTestsNumber());
+        statistic.setProbWinShPath((double) statistic.getWinOutcomeNumShPath() / statistic.getTestsNumber());
 
         statistic.setAvStepNumBacktrack(statistic.getAvStepNumBacktrack() / statistic.getWinOutcomeNumBacktrack());
-        statistic.setAvTimeBacktrack(statistic.getAvTimeBacktrack() / statistic.getWinOutcomeNumBacktrack());
-
         statistic.setAvStepNumAStar(statistic.getAvStepNumAStar() / statistic.getWinOutcomeNumAStar());
+        statistic.setAvStepNumShPath(statistic.getAvStepNumShPath() / statistic.getWinOutcomeNumShPath());
+
+        statistic.setAvTimeBacktrack(statistic.getAvTimeBacktrack() / statistic.getWinOutcomeNumBacktrack());
         statistic.setAvTimeAStar(statistic.getAvTimeAStar() / statistic.getWinOutcomeNumAStar());
+        statistic.setAvTimeShPath(statistic.getAvTimeShPath() / statistic.getWinOutcomeNumShPath());
     }
 
 
@@ -567,7 +578,6 @@ class Actor extends Cell{
         return path;
     }
 
-
     /**
      *  This is the function that uses backtracking
      *  taking into account the features of the map and scenario
@@ -633,7 +643,6 @@ class Actor extends Cell{
         System.out.println("\nOutcome: " + Harry.getOutcome());
         System.out.println("Number of steps: " + Harry.getStep());
         System.out.println("Time taken to reach the door: " + Harry.getTimeTaken() + " nanoseconds\n");
-//        printMap();
     }
 
 
@@ -735,13 +744,18 @@ class Actor extends Cell{
         System.out.println("Time taken to reach the door: " + Harry.getTimeTaken() + " nanoseconds\n");
     }
 
-    public static void findPathToCloak(Actor Harry, Cell Filch, Cell Cat, Cell Cloak, int scenario){
+    /**
+     *  This is the function that uses A* algorithm
+     *  taking into account the features of the map and scenario
+     *  to find path to the cloakS
+     *
+     * @return boolean true if it found path to the cloak
+     */
+
+    public static boolean findPathToCloak(Actor Harry, Cell Filch, Cell Cat, Cell Cloak, int scenario){
         long time0 = System.nanoTime();
         ArrayList<Cell> path = aStar(Harry, Cloak);
         Harry.setTimeTaken(Harry.getTimeTaken() + System.nanoTime() - time0);
-        ArrayList<Cell> visitedCells = new ArrayList<>();
-
-        visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
 
         while (Harry.getX() != Cloak.getX() || Harry.getY() != Cloak.getY()) {
             checkCloak(Harry, Cloak);
@@ -750,31 +764,31 @@ class Actor extends Cell{
             long time1 = System.nanoTime();
             path = aStar(Harry, Cloak);
             Harry.setTimeTaken(Harry.getTimeTaken() + System.nanoTime() - time1);
-            if (path == null) {
+            if (path == null || path.size() == 0) {
                 Harry.setOutcome("Lose");
-                System.out.println("\nOutcome: " + Harry.getOutcome() + " (Impossible to reach exit)");
+                System.out.println("Outcome: " + Harry.getOutcome() + " (Impossible to reach exit)");
                 System.out.println("Number of steps: " + Harry.getStep());
                 System.out.println("Time taken to reach the door: Harry will never reach exit\n");
-                return;
+                return false;
             }
             Harry.setX(path.get(0).getX());
             Harry.setY(path.get(0).getY());
             if (isInCatZone(Harry.getX(), Harry.getY(), Harry, Cat) || isInFilchZone(Harry.getX(), Harry.getY(), Harry, Filch)) {
                 Harry.setStep(Harry.getStep() + 1);
                 Harry.setOutcome("Lose");
-                System.out.println("\nOutcome: " + Harry.getOutcome() + " (Harry has been caught by inspector)");
+                System.out.println("Outcome: " + Harry.getOutcome() + " (Harry has been caught by inspector)");
                 System.out.println("Number of steps: " + Harry.getStep());
                 System.out.println("Time taken to reach the door: Harry will never reach exit");
-                return;
+                return false;
             }
             Harry.setStep(Harry.getStep() + 1);
             path.remove(0);
-            visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
             System.out.print("[" + Harry.getX() + "," + Harry.getY() + "]");
             checkCloak(Harry, Cloak);
             senseCatZone(Harry, Cat, scenario);
             senseFilchZone(Harry, Filch, scenario);
         }
+        return true;
     }
 
 
@@ -786,9 +800,6 @@ class Actor extends Cell{
         long time0 = System.nanoTime();
         ArrayList<Cell> path = aStar(Harry, Book);
         Harry.setTimeTaken(Harry.getTimeTaken() + System.nanoTime() - time0);
-        ArrayList<Cell> visitedCells = new ArrayList<>();
-
-        visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
 
         while (Harry.getX() != Book.getX() || Harry.getY() != Book.getY()) {
             checkBook(Harry, Book);
@@ -800,14 +811,15 @@ class Actor extends Cell{
             Harry.setTimeTaken(Harry.getTimeTaken() + System.nanoTime() - time1);
             if (path == null) {
                 Harry.setOutcome("Lose");
-                System.out.println("\nOutcome: " + Harry.getOutcome() + " (Impossible to reach exit)");
+                System.out.println("Outcome: " + Harry.getOutcome() + " (Impossible to reach exit)");
                 System.out.println("Number of steps: " + Harry.getStep());
                 System.out.println("Time taken to reach the door: Harry will never reach exit\n");
                 return;
             }
             if (path.size() == 0){
-                findPathToCloak(Harry, Filch, Cat, Cloak, scenario);
-                visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
+                if (!findPathToCloak(Harry, Filch, Cat, Cloak, scenario)){
+                    return;
+                }
                 senseCatZone(Harry, Cat, scenario);
                 senseFilchZone(Harry, Filch, scenario);
                 long timeIn = System.nanoTime();
@@ -819,14 +831,13 @@ class Actor extends Cell{
             if (isInCatZone(Harry.getX(), Harry.getY(), Harry, Cat) || isInFilchZone(Harry.getX(), Harry.getY(), Harry, Filch)) {
                 Harry.setStep(Harry.getStep() + 1);
                 Harry.setOutcome("Lose");
-                System.out.println("\nOutcome: " + Harry.getOutcome() + " (Harry has been caught by inspector)");
+                System.out.println("Outcome: " + Harry.getOutcome() + " (Harry has been caught by inspector)");
                 System.out.println("Number of steps: " + Harry.getStep());
                 System.out.println("Time taken to reach the door: Harry will never reach exit");
                 return;
             }
             Harry.setStep(Harry.getStep() + 1);
             path.remove(0);
-            visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
             System.out.print("[" + Harry.getX() + "," + Harry.getY() + "]");
             checkBook(Harry, Book);
             checkCloak(Harry, Cloak);
@@ -834,7 +845,6 @@ class Actor extends Cell{
             senseFilchZone(Harry, Filch, scenario);
         }
 
-        visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
         while (Harry.getX() != Exit.getX() || Harry.getY() != Exit.getY()) {
             checkCloak(Harry, Cloak);
             senseCatZone(Harry, Cat, scenario);
@@ -844,14 +854,16 @@ class Actor extends Cell{
             Harry.setTimeTaken(Harry.getTimeTaken() + System.nanoTime() - time1);
             if (path == null) {
                 Harry.setOutcome("Lose");
-                System.out.println("\nOutcome: " + Harry.getOutcome() + " (Impossible to reach exit)");
+                System.out.println("Outcome: " + Harry.getOutcome() + " (Impossible to reach exit)");
                 System.out.println("Number of steps: " + Harry.getStep());
                 System.out.println("Time taken to reach the door: Harry will never reach exit\n");
                 return;
             }
             if (path.size() == 0){
                 findPathToCloak(Harry, Filch, Cat, Cloak, scenario);
-                visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
+                if (!findPathToCloak(Harry, Filch, Cat, Cloak, scenario)){
+                    return;
+                }
                 senseCatZone(Harry, Cat, scenario);
                 senseFilchZone(Harry, Filch, scenario);
                 long timeIn = System.nanoTime();
@@ -863,14 +875,13 @@ class Actor extends Cell{
             if (isInCatZone(Harry.getX(), Harry.getY(), Harry, Cat) || isInFilchZone(Harry.getX(), Harry.getY(), Harry, Filch)) {
                 Harry.setStep(Harry.getStep() + 1);
                 Harry.setOutcome("Lose");
-                System.out.println("\nOutcome: " + Harry.getOutcome() + " (Harry has been caught by inspector)");
+                System.out.println("Outcome: " + Harry.getOutcome() + " (Harry has been caught by inspector)");
                 System.out.println("Number of steps: " + Harry.getStep());
                 System.out.println("Time taken to reach the door: Harry will never reach exit");
                 return;
             }
             Harry.setStep(Harry.getStep() + 1);
             path.remove(0);
-            visitedCells.add(new Cell(Harry.getX(), Harry.getY()));
             System.out.print("[" + Harry.getX() + "," + Harry.getY() + "]");
             checkCloak(Harry, Cloak);
             senseCatZone(Harry, Cat, scenario);
@@ -881,7 +892,6 @@ class Actor extends Cell{
         System.out.println("\nOutcome: " + Harry.getOutcome());
         System.out.println("Number of steps: " + Harry.getStep());
         System.out.println("Time taken to reach the door: " + Harry.getTimeTaken() + " nanoseconds");
-        return;
     }
 }
 
@@ -896,26 +906,32 @@ class Statistic{
     private int testsNumber;
 
     /**
-     *  These 2 entities are for counting the probability that Actor will win the game
-     *  using A* algorithm and using Backtracking algorithm
+     *  These 4 entities are for counting the probability that Actor will win the game
+     *  using A* algorithm and using Backtracking algorithm and using Shortest path
      */
     private double probWinAStar;
     private double probWinBacktrack;
+    private double probWinShPath;
 
     /**
-     *  These 2 entities are for counting how many winning outcomes using A* algorithm and Backtracking algorithm
+     *  These 3 entities are for counting how many winning outcomes
+     *  using A* algorithm and Backtracking algorithm and Shortest path
      */
     private int winOutcomeNumAStar;
     private int winOutcomeNumBacktrack;
+    private int winOutcomeNumShPath;
 
     /**
-     *  These 4 entities are for counting average value of steps
-     *  and time taken to reach the book and the exit using A* algorithm and Backtracking algorithms
+     *  These 6 entities are for counting average value of steps
+     *  and time taken to reach the book and the exit using
+     *  A* algorithm and Backtracking algorithms and Shortest path
      */
     private double avStepNumAStar;
     private double avStepNumBacktrack;
+    private double avStepNumShPath;
     private long avTimeAStar;
     private long avTimeBacktrack;
+    private long avTimeShPath;
 
     /**
      *  This is the constructor of class Statistic which helps to create objects
@@ -924,7 +940,7 @@ class Statistic{
 
     public Statistic(int testsNumber, double probWinAStar, double probWinBacktrack,
                      int winOutcomeNumAStar, int winOutcomeNumBacktrack, double avStepNumAStar, double avStepNumBacktrack,
-                     long avTimeAStar, long avTimeBacktrack){
+                     long avTimeAStar, long avTimeBacktrack, double avStepNumShPath, long avTimeShPath){
         this.testsNumber = testsNumber;
         this.probWinAStar = probWinAStar;
         this.probWinBacktrack = probWinBacktrack;
@@ -934,6 +950,8 @@ class Statistic{
         this.avStepNumBacktrack = avStepNumBacktrack;
         this.avTimeAStar = avTimeAStar;
         this.avTimeBacktrack = avTimeBacktrack;
+        this.avStepNumShPath = avStepNumShPath;
+        this.avTimeShPath = avTimeShPath;
     }
 
     /**
@@ -947,17 +965,25 @@ class Statistic{
 
     public double getProbWinBacktrack() {return probWinBacktrack;}
 
+    public double getProbWinShPath() {return probWinShPath;}
+
     public int getWinOutcomeNumAStar() {return winOutcomeNumAStar;}
 
     public int getWinOutcomeNumBacktrack() {return winOutcomeNumBacktrack;}
+
+    public int getWinOutcomeNumShPath(){return winOutcomeNumShPath;}
 
     public double getAvStepNumAStar() {return avStepNumAStar;}
 
     public double getAvStepNumBacktrack() {return avStepNumBacktrack;}
 
+    public double getAvStepNumShPath() {return avStepNumShPath;}
+
     public long getAvTimeAStar() {return avTimeAStar;}
 
     public long getAvTimeBacktrack() {return avTimeBacktrack;}
+
+    public long getAvTimeShPath() {return avTimeShPath;}
 
     public void setTestsNumber(int testsNumber) {this.testsNumber = testsNumber;}
 
@@ -965,39 +991,50 @@ class Statistic{
 
     public void setProbWinBacktrack(double probWinBacktrack) {this.probWinBacktrack = probWinBacktrack;}
 
+    public void setProbWinShPath(double probWinShPath) {this.probWinShPath = probWinShPath;}
+
     public void setWinOutcomeNumAStar(int winOutcomeNumAStar) {this.winOutcomeNumAStar = winOutcomeNumAStar;}
 
     public void setWinOutcomeNumBacktrack(int winOutcomeNumBacktrack) {this.winOutcomeNumBacktrack = winOutcomeNumBacktrack;}
+
+    public void setWinOutcomeNumShPath(int winOutcomeNumShPath) {this.winOutcomeNumShPath = winOutcomeNumShPath;}
 
     public void setAvStepNumAStar(double avStepNumAStar) {this.avStepNumAStar = avStepNumAStar;}
 
     public void setAvStepNumBacktrack(double avStepNumBacktrack) {this.avStepNumBacktrack = avStepNumBacktrack;}
 
+    public void setAvStepNumShPath(double avStepNumShPath) {this.avStepNumShPath = avStepNumShPath;}
+
     public void setAvTimeAStar(long avTimeAStar) {this.avTimeAStar = avTimeAStar;}
 
     public void setAvTimeBacktrack(long avTimeBacktrack) {this.avTimeBacktrack = avTimeBacktrack;}
+
+    public void setAvTimeShPath(long avTimeShPath) {this.avTimeShPath = avTimeShPath;}
 
 
     /**
      *  This is the function which print main statistics information
      */
     public void printStatistic(){
-        if (getTestsNumber() == 0 && getProbWinBacktrack() == 0 &&
-                getProbWinAStar() == 0 && getWinOutcomeNumBacktrack() == 0 && getWinOutcomeNumAStar() == 0 && getAvStepNumBacktrack() == 0 &&
-                getAvStepNumAStar() == 0 && getAvTimeBacktrack() == 0 && getAvTimeAStar() == 0){
+        if (getTestsNumber() == 0 &&
+                getProbWinBacktrack() == 0 && getProbWinAStar() == 0 && getProbWinShPath() == 0 &&
+                getWinOutcomeNumBacktrack() == 0 && getWinOutcomeNumAStar() == 0 && getWinOutcomeNumShPath() == 0 &&
+                getAvStepNumBacktrack() == 0 && getAvStepNumAStar() == 0 && getAvStepNumShPath() == 0 &&
+                getAvTimeBacktrack() == 0 && getAvTimeAStar() == 0 && getAvTimeShPath() == 0){
             System.out.println("Statistical analysis only for big amount of tests.\nPlease use generateTests() function to know more about statistics.");
             return;
         }
         System.out.println("\nStatistical Analysis:");
-        System.out.println("Number of tests: " +  + getTestsNumber());
+        System.out.println("Number of tests: " + getTestsNumber());
         System.out.printf("Probability of winning test using Backtracking method: %.3f %n", getProbWinBacktrack());
-        System.out.printf("Probability of winning test using A* method: %.3f %n", getProbWinAStar());
+        System.out.printf("Probability of winning test using A* method: %.3f %n\n", getProbWinAStar());
         System.out.println("There was " + getWinOutcomeNumBacktrack() + " winning outcomes when Backtracking method was used");
-        System.out.println("There was " + getWinOutcomeNumAStar() + " winning outcomes when A* method was used");
+        System.out.println("There was " + getWinOutcomeNumAStar() + " winning outcomes when A* method was used\n");
         System.out.printf("There were %.3f steps on average to reach exit using Backtracking method %n", getAvStepNumBacktrack());
         System.out.printf("There were %.3f steps on average to reach exit using A* method %n", getAvStepNumAStar());
+        System.out.printf("There were %.3f steps on average to reach exit using Shortest path %n\n", getAvStepNumShPath());
         System.out.println("On average, " + getAvTimeBacktrack() + " nanoseconds needed to reach exit using Backtracking method");
         System.out.println("On average, " + getAvTimeAStar() + " nanoseconds needed to reach exit using A* method");
-        //Shortest path to do
+        System.out.println("On average, " + getAvTimeShPath() + " nanoseconds needed to reach exit using Shortest path");
     }
 }
